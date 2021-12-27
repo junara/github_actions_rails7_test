20c5ea7ae4ffc6ead51bef7bd9a806f3a330735c
junara <jung5araki@gmail.com>
2021-12-28 08:45:47 +0900

    fail test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe '#valid?' do
     it { expect(described_class.new(name: 'hoge')).to be_valid }
-    it { expect(described_class.new(name: '')).to be_invalid }
+    it { expect(described_class.new(name: '')).to be_valid }
   end
 end
